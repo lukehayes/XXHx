268f8f2fa973d7c26558ec3173fbf97c796b9f7a
lukehayes <lukepurplehayes@gmail.com>
2024-09-10 17:24:02 +0100

    First Commit

---
+++ src/xx/Game.hx
@@ -0,0 +1,17 @@
+package xx;
+
+class Game extends hxd.App
+{
+    override function init() 
+    {
+    }
+
+    override function update(dt:Float) 
+    {
+    }
+
+    static function main() {
+        hxd.Res.initEmbed();
+        new Game();
+    }
+}
