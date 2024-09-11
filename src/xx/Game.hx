f4697e45b34832dc3e47ad32dced85b52af4686c
lukehayes <lukepurplehayes@gmail.com>
2024-09-11 09:52:43 +0100

    Added test square to Game.hx

--- src/xx/Game.hx
+++ src/xx/Game.hx
@@ -2,12 +2,22 @@ package xx;
 
 class Game extends hxd.App
 {
+    var gfx : h2d.Graphics;
+    static var c = 0.0;
+
     override function init() 
     {
+        this.gfx = new h2d.Graphics(s2d);
     }
 
     override function update(dt:Float) 
     {
+        c += 0.01;
+
+        gfx.clear();
+        gfx.beginFill(0xFF00FF);
+            gfx.drawRect(100 + Math.sin(c) * 100,100 + Math.cos(c) * 100,100,100);
+        gfx.endFill();
     }
 
     static function main() {
