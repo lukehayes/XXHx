b880e8d6115fdbc37d876e0b1c22ef8912cadc7f
lukehayes <lukepurplehayes@gmail.com>
2024-09-14 11:08:26 +0100

    Implemented start of Entity class

--- src/xx/Game.hx
+++ src/xx/Game.hx
@@ -1,25 +1,50 @@
 package xx;
 
 import xx.math.Vec2;
+import xx.entity.Entity;
 
 class Game extends hxd.App
 {
     var gfx : h2d.Graphics;
     static var c = 0.0;
+    
+    var entities : Array<Entity>;
 
     override function init() 
     {
         this.gfx = new h2d.Graphics(s2d);
+
+        this.entities = new Array<Entity>();
+        this.entities.push(new Entity(
+            new Vec2(100,400),
+            new Vec2(32,32)
+        ));
+
+        this.entities.push(new Entity(
+            new Vec2(400,100),
+            new Vec2(32,32)
+        ));
+
+        this.entities.push(new Entity(
+            new Vec2(200,210),
+            new Vec2(32,64)
+        ));
     }
 
     override function update(dt:Float) 
     {
-        c += 0.1;
+        for(e in this.entities)
+        {
+            e.update(dt);
+        }
 
         gfx.clear();
-        gfx.beginFill(0xAA00FF);
-            gfx.drawRect(300 + Math.sin(c) * 100,300 + Math.cos(c) * 100,100,100);
-        gfx.endFill();
+        for(e in this.entities)
+        {
+            e.draw(gfx);
+        }
+
+
     }
 
     static function main() {
--- src/xx/entity/Entity.hx
+++ src/xx/entity/Entity.hx
@@ -1,16 +1,39 @@
-package entity;
+package xx.entity;
 
-import math.Vec2;
+import xx.math.Vec2;
 
 class Entity
 {
-    var position : Vec2;
+    public var position : Vec2;
+    public var scale    : Vec2;
+    public var color    = 0xFF00FF;
 
-    public function new(pos: Vec2)
+    public function new(pos: Vec2, scale: Vec2)
     {
         this.position = pos;
-        trace("Entity Created");
+        this.scale    = scale;
     }
+
+    public function update(dt:Float)
+    {
+        static var c = 0.0;
+        c += 0.1;
+
+        position.x = this.position.x + Math.cos(c) * 10;
+    }
+
+    public function draw(gfx: h2d.Graphics)
+    {
+        gfx.beginFill(this.color);
+            gfx.drawRect(
+                this.position.x,
+                this.position.y,
+                this.scale.x,
+                this.scale.y
+            );
+        gfx.endFill();
+    }
+
 }
 
 
